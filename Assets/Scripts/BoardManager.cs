bcdf80e4b34d8e1493ca2283bd8b5b7af7018655
j-landers <gymbolanders@gmail.com>
2016-09-26 11:24:43 -0700

    day 3
    10 errors in boardmanager script, need to be fixed.

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -37,15 +37,61 @@ public class BoardManager : MonoBehaviour
 		gridPositions.Clear ();
 
 		for (int x = 1; x < columns - 1; x++) 
+		{
+			for (int y = 1; y < rows - 1; y++)
+			{
+				gridPositions.Add(new Vector3(x, y, 0f));
+			}
+		}
 	}
 
-	// Use this for initialization
-	void Start () {
-	
+	void BoardSetup ()
+	{
+		boardHolder = new GameObject ("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++) {
+		{
+			for (int y = -1; y < rows + 1; y++) 
+			{
+				GameObject toInstantiate = floorTiles[Random.Range (0, floorTiles.Length];
+					if (x == -1 || x == columns || y == -1 || y == rows)
+						toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
+
+				GameObject instance = Instantiate(toInstantiate, new Vector3 (x,y,0f), Quaternion.identity as GameObject;
+
+				instance.transform.SetParent(boardHolder);
+			}
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	Vector3 RandomPosition()
+	{
+		int randomIndex = Random.RangeAttribute(0, gridPositions.Count);
+		Vector3 randomPosition  = gridPositions[randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
 	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+	{
+		int objectCount = Random.RangeAttribute (minumum, maximum +1);
+
+		for (int i = 0; if < objectCount; i++)
+		{
+			Vector3 randomPosition = RandomPosition();						
+			GameObject tileChoice = tileArray[Random.RangeAttribute (0, tileArray.Length)];
+			Instantiate (tileChoice, randomPosition, Quaternion.identity);
+		}							
+	}
+
+	public void SetupScene (int level)
+	{
+		BoardSetup();
+		InitialiseList();
+		LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Mathf.Logger(QualityLevel,2f);
+		LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+		Instantiate(exit, new Vector3(columns - 1, rows -1, 0f), Quaternion.identity);
+
 }
